205ab80e2735805bbd7842905c779a3fed1a0c2a
HRISTOV <Y.HRISTOV@iaea.org>
2018-03-28 18:07:31 +0200

    Improve sync

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#import discord # learn more: https://python.org/pypi/discord
+# import discord # learn more: https://python.org/pypi/discord
 from discord_hooks import Webhook
 from telethon import TelegramClient, events
 from telethon.tl.types import PeerChannel
@@ -9,7 +9,8 @@ api_id = os.getenv('TELEGRAM_API_ID')
 api_hash = os.getenv('TELEGRAM_API_HASH')
 phone = os.getenv('PHONE_NUMBER')
 discord_webhook_url = os.getenv('DISCORD_WEBHOOK_URL')
-telegram_chats = os.getenv('TELEGRAM_CHATS', 'SharkSniper,CoinSniper,WallMonitor').split(',')
+telegram_chats = os.getenv(
+    'TELEGRAM_CHATS', 'SharkSniper,CoinSniper,WallMonitor').split(',')
 
 logging.basicConfig(
     level=logging.INFO,
@@ -17,7 +18,8 @@ logging.basicConfig(
     datefmt='%Y-%m-%d %H:%M:%S')
 logger = logging.getLogger(__name__)
 
-client = TelegramClient(phone, api_id, api_hash, update_workers=4, spawn_read_thread=False)
+client = TelegramClient(phone, api_id, api_hash,
+                        update_workers=4, spawn_read_thread=False)
 client.start()
 
 chats = []
@@ -27,15 +29,23 @@ for chat in telegram_chats:
     chats.append(chat)
 print('Channels: ', chats)
 
+
 @client.on(events.NewMessage(chats=chats, incoming=True))
 def event_handler(event):
-    msg = Webhook(discord_webhook_url, msg=event.raw_text)
+    msg_text = event.text  # event.raw_text
+    import re
+    url_reg = r'\((http[^)]+)\)'
+    msg_text = re.sub(url_reg, r'(<\1>)', msg_text)
+    msg = Webhook(discord_webhook_url, msg=msg_text)
+    # print(event)
+    print(event.input_sender, event.document, event.text)
     msg.post()
+
 
 client.idle()
 client.disconnect()
 
-### Resources
+# Resources
 # https://www.devdungeon.com/content/discord-webhook-tutorial-check-bitcoin-price-python
 # https://www.devdungeon.com/content/make-discord-bot-python-part-2
 # https://hackaday.com/2018/02/15/creating-a-discord-webhook-in-python/
---
+++ test.py
@@ -0,0 +1,15 @@
+source_text = '''
+text1
+text2
+http://url.com/bla1/blah1/
+text3
+text4
+[label](http://url.com/bla2/blah2/)
+text5
+text6    '''
+
+import re
+#url_reg = r'\((http[^)]+)\)'
+url_reg = r'([a-z]*[:.]+\S+)'
+result = re.sub(url_reg, r'<\1>', source_text)
+print(result)
